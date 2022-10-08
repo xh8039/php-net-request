86f8934c817c96618070ecdf70dc46de6c6c0fe3
易航网络 <xh118039@qq.com>
2022-10-08 23:22:42 +0800

    更新类名curl为Curl，将动态curl类优化为可直接调用的静态Curl类

--- curl.php
+++ curl.php
@@ -1,15 +1,15 @@
 <?php
 
-class curl
+class Curl
 {
-	public $ch;
+	private static $ch;
 
 	/**
 	 * 请求配置
 	 * @access public
 	 * @return array
 	 */
-	public $requsetConfig = [
+	private static $requset_config = [
 		'header' => [
 			'Accept: */*',
 			'Accept-Encoding: gzip,deflate,sdch',
@@ -30,74 +30,77 @@ class curl
 	 * @access public
 	 * @return array
 	 */
-	public $responseContent = [];
+	public static $requset_content = [];
 
-	public function __construct()
+	private static function init()
 	{
-		$this->ch = curl_init();
-		curl_setopt($this->ch, CURLOPT_SSL_VERIFYPEER, false);
-		curl_setopt($this->ch, CURLOPT_SSL_VERIFYHOST, false);
-		curl_setopt($this->ch, CURLOPT_ENCODING, "gzip");
-		curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, 1);
-		curl_setopt($this->ch, CURLOPT_HEADER, 1);
+		self::$ch = curl_init();
+		curl_setopt(self::$ch, CURLOPT_SSL_VERIFYPEER, false);
+		curl_setopt(self::$ch, CURLOPT_SSL_VERIFYHOST, false);
+		curl_setopt(self::$ch, CURLOPT_ENCODING, "gzip");
+		curl_setopt(self::$ch, CURLOPT_RETURNTRANSFER, 1);
+		curl_setopt(self::$ch, CURLOPT_HEADER, 1);
+		curl_setopt(self::$ch, CURLOPT_HTTPHEADER, self::$requset_config['header']);  // 请求头
+		curl_setopt(self::$ch, CURLOPT_CONNECTTIMEOUT, self::$requset_config['connect_time']);  // 请求时间
+		curl_setopt(self::$ch, CURLOPT_TIMEOUT, self::$requset_config['read_time']); // 读取时间
 	}
 
 	/**
 	 * 配置请求参数
 	 * @access public
 	 * @param array $config 要配置的数组请求参数
-	 * @return $this
+	 * @return self
 	 */
-	public function requestConfig($config)
+	public static function requestConfig($config)
 	{
 		foreach ($config as $key => $value) {
 			if (!empty($value)) {
-				$this->requsetConfig[$key] = $value;
+				self::$requset_config[$key] = $value;
 			}
 		}
-		curl_setopt($this->ch, CURLOPT_HTTPHEADER, $this->requsetConfig['header']);  // 请求头
-		curl_setopt($this->ch, CURLOPT_CONNECTTIMEOUT, $this->requsetConfig['connect_time']);  // 请求时间
-		curl_setopt($this->ch, CURLOPT_TIMEOUT, $this->requsetConfig['read_time']); // 读取时间
-		return $this;
+		return self::class;
 	}
 
 	/**
 	 * GET请求
 	 * @access public
 	 * @param string $url 请求URL
-	 * @return mixed
+	 * @return string
 	 */
-	public function get($url)
+	public static function get($url)
 	{
-		$this->requsetConfig['param'] = http_build_query($this->requsetConfig['param']);
-		$url = strstr($url, '?') ? trim($url, '&') . '&' .  $this->requsetConfig['param'] : $url . '?' .  $this->requsetConfig['param'];
-		return $this->request($url);
+		if (self::$requset_config['param']) {
+			self::$requset_config['param'] = http_build_query(self::$requset_config['param']);
+			$url = strstr($url, '?') ? trim($url, '&') . '&' .  self::$requset_config['param'] : $url . '?' .  self::$requset_config['param'];
+		}
+		return self::request($url);
 	}
 
 	/**
 	 * POST请求
 	 * @access public
 	 * @param string $url 请求URL
-	 * @return mixed
+	 * @return string
 	 */
-	public function post($url)
+	public static function post($url)
 	{
-		curl_setopt($this->ch, CURLOPT_POST, 1);
-		curl_setopt($this->ch, CURLOPT_POSTFIELDS, $this->requsetConfig['param']);
-		return $this->request($url);
+		curl_setopt(self::$ch, CURLOPT_POST, 1);
+		self::$requset_config['param'] ? curl_setopt(self::$ch, CURLOPT_POSTFIELDS, self::$requset_config['param']) : null;
+		return self::request($url);
 	}
 
 	/**
 	 * 处理请求
-	 * @access public
+	 * @access private
 	 * @param string $url 请求URL
-	 * @return mixed
+	 * @return string
 	 */
-	private function request($url)
+	private static function request($url)
 	{
-		curl_setopt($this->ch, CURLOPT_URL, $url);
-		$response_body = curl_exec($this->ch);
-		$header_size = curl_getinfo($this->ch, CURLINFO_HEADER_SIZE);
+		self::init();
+		curl_setopt(self::$ch, CURLOPT_URL, $url);
+		$response_body = curl_exec(self::$ch);
+		$header_size = curl_getinfo(self::$ch, CURLINFO_HEADER_SIZE);
 		$header = substr($response_body, 0, $header_size);
 		$header_explode = explode(PHP_EOL, $header);
 		$header_explodes = [];
@@ -122,11 +125,34 @@ class curl
 		];
 		$body = substr($response_body, $header_size);
 		$results = [
-			'body' => $body, 'header' => $headers, 'code' => curl_getinfo($this->ch, CURLINFO_HTTP_CODE),
+			'body' => $body, 'header' => $headers, 'code' => curl_getinfo(self::$ch, CURLINFO_HTTP_CODE),
 		];
-		curl_close($this->ch);
-		$this->responseContent = $results;
-		$this->ch = curl_init();
+		curl_close(self::$ch);
+		self::$requset_content = $results;
+		self::recovery();
 		return $results['body'];
+	}
+
+	/**
+	 * 恢复Curl静态类
+	 * @access private
+	 */
+	private static function recovery() {
+		self::$ch = curl_init();
+		self::$requset_config = [
+			'header' => [
+				'Accept: */*',
+				'Accept-Encoding: gzip,deflate,sdch',
+				'Accept-Language: zh-CN,zh;q=0.9,en;q=0.8,en-GB;q=0.7,en-US;q=0.6',
+				'Connection:close',
+				'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/104.0.5112.102 Safari/537.36 Edg/104.0.1293.70'
+			],
+			'param' => null,
+			'cookie' => null,
+			// 连接时间
+			'connect_time' => 30,
+			// 读取时间
+			'read_time' => null
+		];
 	}
 }
--- index.php
+++ index.php
@@ -1,12 +1,14 @@
 <?php
-include 'curl.php';
-$curl = new curl;
-$curl->requestConfig([
+include 'Curl.php';
+$get = Curl::requestConfig([
 	'param' => [
 		'a' => 000,
 		'b' => 333
 	]
-]);
-
-$get = $curl->get('http://blog.bri6.cn');
-print_r($curl->responseContent);
+])::get('http://blog.bri6.cn');
+print_r($get);
+print_r(PHP_EOL);
+print_r(Curl::$requset_content);
+print_r(PHP_EOL . PHP_EOL);
+$get = Curl::get('http://www.bri6.cn');
+print_r($get);
