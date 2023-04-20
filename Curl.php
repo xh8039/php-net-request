92456e558915c38085c04e778280ef1b500b4bea
yh_IT <2136118039@qq.com>
2023-04-20 22:26:16 +0800

    111

--- Curl.php
+++ Curl.php
@@ -29,9 +29,9 @@ class Curl
 			'params' => [],
 			'cookie' => null,
 			// 连接时间
-			'connect_time' => 5,
+			'connect_time' => 3,
 			// 读取时间
-			'read_time' => 10
+			'read_time' => 5
 		]
 	];
 
@@ -169,104 +169,5 @@ class Curl
 			'code' => $http_code
 		];
 		return new response($response);
-	}
-}
-
-class response
-{
-	private $response;
-
-	private $headers = null;
-
-	public function __construct($response)
-	{
-		$this->response = $response;
-	}
-
-	/**
-	 * 直接获取响应体内容
-	 * @return string
-	 */
-	public function __toString()
-	{
-		return $this->response['body'];
-	}
-
-	/**
-	 * 直接获取响应体内容
-	 * @return string
-	 */
-	public function body()
-	{
-		return $this->__toString();
-	}
-
-	/**
-	 * 获取响应状态码
-	 * @return integer
-	 */
-	public function code()
-	{
-		return trim($this->response['code']);
-	}
-
-	/**
-	 * 获取指定响应头信息
-	 * @access public
-	 * @param $name 响应头名称
-	 * @return string
-	 */
-	public function header($name)
-	{
-		$headers = $this->headers();
-		$name = strtolower(trim($name));
-		return isset($headers[$name]) ? $headers[$name] : null;
-	}
-
-	/**
-	 * 获取数组形式的所有响应头信息
-	 * @return array
-	 */
-	public function headers()
-	{
-		if (is_null($this->headers)) {
-			$this->headers = $this->explodeHeaders($this->response['headers']);
-		}
-		return $this->headers;
-	}
-
-	/**
-	 * 将获取到的JSON数据转换为PHP数组
-	 * @access public
-	 * @return array
-	 */
-	public function toArray()
-	{
-		return json_decode($this->response['body'], true);
-	}
-
-	/**
-	 * 将获取到的JSON数据转换为PHP对象
-	 * @access public
-	 * @return object
-	 */
-	public function toObject()
-	{
-		return json_decode($this->response['body']);
-	}
-
-	private function explodeHeaders(array $headers)
-	{
-		$headers_array = [];
-		$headers[] = 'data : :aa:bb: ';
-		foreach ($headers as $value) {
-			if (strpos($value, ':')) {
-				$value = explode(':', $value, 2);
-				$headers_array[strtolower(trim($value[0]))] = trim($value[1]);
-			} else {
-				$headers_array[] = $value;
-			}
-		}
-		return $headers_array;
 	}
 }
